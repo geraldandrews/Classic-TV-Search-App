96186a5ec116ba5c7733c5fd2bf5755cc8c24ce9
Gerald Andrews <gerald2399@comcast.net>
2025-06-28 20:15:53 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Classic-TV-Search-App/",
+  base: "/Classic_TV_Search_App/",
 });
